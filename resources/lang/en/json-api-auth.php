faa89db89040d0aea9cd6d8219153c9bb76f13f2
JaviPtz <jam.pastaz@yavirac.deu.ec>
2021-10-05 17:32:14 -0500

    autenticacion con json api

---
+++ resources/lang/en/json-api-auth.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Json Api Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => ' La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en: segundos segundos.',
+    'success' => 'éxito.',
+    'logout' => 'cerraste sesión correctamente.',
+    'email_already_verified' => 'Correo electrónico ya ha sido verificado.',
+    'email_verified' => 'Correo Electrónico Verificado.',
+    'email_sent' => 'Email enviado, revisao tu correo.',
+    'password_confirmed' => 'Contraseña confirmada.',
+    'password_updated' => 'Contraseña actualizada.',
+    'check_your_email' => 'Consultar su correo electrónico',
+
+];
